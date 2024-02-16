14921a3d39b7e4ed5c01cd1342226b0d98b40887
ViTienDat <datndls0703@gmail.com>
2024-02-16 13:56:02 +0700

    deploy 3

--- src/containers/public/Home.jsx
+++ src/containers/public/Home.jsx
@@ -5,6 +5,8 @@ import {
   Section2,
   NewReLease,
   ChartSection,
+  LoadingData,
+  Loading,
 } from "../../components";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -13,33 +15,45 @@ const Home = () => {
   const { seasontheme, weekchart, heditortheme3, heditortheme, top100 } =
     useSelector((state) => state.app);
   return (
-    <div className="overflow-y-auto bg-[#170f23] px-[50px] text-[#DADADA] flex flex-col ">
-      <div className=" text-white w-full">
-        <Sliders />
-        <NewReLease />
-        <Section data={seasontheme} />
-        <Section2 data={top100} />
-        <ChartSection />
-        <div className="flex gap-6 mt-10">
-          {weekchart?.items?.map((item) => (
-            <Link
-              to={item?.link?.split(".")[0]}
-              key={item.link}
-              className="flex-1"
-            >
-              <img
-                src={item?.cover}
-                alt="cover"
-                className="w-full object-cover rounded-md"
-              />
-            </Link>
-          ))}
+    <>
+      {seasontheme !== null &&
+      weekchart !== null &&
+      heditortheme3 !== null &&
+      heditortheme !== null &&
+      top100 !== null ? (
+        <div className="overflow-y-auto bg-[#170f23] px-[50px] text-[#DADADA] flex flex-col ">
+          <div className=" text-white w-full">
+            <Sliders />
+            <NewReLease />
+            <Section data={seasontheme} />
+            <Section2 data={top100} />
+            <ChartSection />
+            <div className="flex gap-6 mt-10">
+              {weekchart?.items?.map((item) => (
+                <Link
+                  to={item?.link?.split(".")[0]}
+                  key={item.link}
+                  className="flex-1"
+                >
+                  <img
+                    src={item?.cover}
+                    alt="cover"
+                    className="w-full object-cover rounded-md"
+                  />
+                </Link>
+              ))}
+            </div>
+            <Section data={heditortheme3} />
+            <Section data={heditortheme} />
+            <div className="h-[150px]"></div>
+          </div>
         </div>
-        <Section data={heditortheme3} />
-        <Section data={heditortheme} />
-        <div className="h-[150px]"></div>
-      </div>
-    </div>
+      ) : (
+        <div className="w-full h-full flex items-center justify-center text-white">
+          <LoadingData />
+        </div>
+      )}
+    </>
   );
 };
 
